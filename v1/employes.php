eb3ffab015254cd72c8b12be7f5d725af04badfb
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-04-18 11:51:46 +0700

    Update File
    - Add function add data employe
    - Add function update data employe

--- v1/employes.php
+++ v1/employes.php
@@ -23,15 +23,58 @@ function get_karyawan($id = 0)
     while ($row = mysqli_fetch_assoc($result)) {
         $respons[] = $row;
     }
+    header("Content-Type:application/json");
     echo json_encode($respons);
 }
 
 // function untuk tambah data
+function insert_employe()
+{
+    global $connection;
+    $data = json_decode(file_get_contents("php://input"), true);
+    $employe_name = $data['employe_name'];
+    $employe_salary = $data['employe_salary'];
+    $employe_age = $data["employe_age"];
+    $employe_mobile_number = $data["employe_mobile_number"];
+    $employe_birthday = $data["employe_birthday"];
+    $employe_addres = $data["employe_addres"];
+    echo $querysql = "INSERT INTO `tb_employe` (`id`, `employe_name`, `employe_salary`, `employe_age`, `employe_mobile_number`, `employe_birthday`, `employe_addres`) VALUES (NULL, '$employe_name', '$employe_salary', ' $employe_age', '$employe_mobile_number', '$employe_birthday', '$employe_addres');";
+    if (mysqli_query($connection, $querysql)) {
+        $respons = array('status' => 1, 'status_message' => 'Employe Added Succesfully.');
+    } else {
+        $respons = array(
+            'status' => 0,
+            'status_message' => 'Employe Addition Failed.'
+        );
+    }
+    header("Content-Type:application/json");
+    echo json_encode($respons);
+}
 
+// function untuk update data
+function update_employe($id)
+{
+    global $connection;
+    $post_vars = json_decode(file_get_contents("php://input"), true);
+    $employe_name = $post_vars['employe_name'];
+    $employe_salary = $post_vars['employe_salary'];
+    $employe_age = $post_vars["employe_age"];
+    $employe_mobile_number = $post_vars["employe_mobile_number"];
+    $employe_birthday = $post_vars["employe_birthday"];
+    $employe_addres = $post_vars["employe_addres"];
+    echo $querysql = "UPDATE `tb_employe` SET `employe_name` = '$employe_name', `employe_salary` = '$employe_salary', `employe_age` = '$employe_age', `employe_mobile_number` = '$employe_mobile_number', `employe_birthday` = '$employe_birthday', `employe_addres` = '$employe_addres' WHERE `tb_employe`.`id` = $id;";
+    if (mysqli_query($connection, $querysql)) {
+        $respons = array('status' => 1, 'status_message' => 'Employe Updated Successfully');
+    } else {
+        $respons = array('status' => 0, 'status_message' => 'Employe Updation Failed ');
+    }
+    header("Content-Type:application/json");
+    echo json_encode($respons);
+}
 
 // disini saya menggunakan method GET
 switch ($request_method) {
-        // untuk method get
+        // untuk method get (show)
     case 'GET':
         // cek apabila id sesuai dengan yang ada di db
         if (!empty($_GET["id"])) {
@@ -41,14 +84,21 @@ switch ($request_method) {
         }
         break;
 
-        // untuk method post    
+        // untuk method post (tambah)
     case 'POST':
 
-        insert_employes();
+        insert_employe();
+        break;
+
+        // untuk method put
+    case 'PUT':
+
+        $id = intval($_GET["id"]);
+        update_employe($id);
         break;
 
     default:
         // invalid request method
-        // header("HTTP/1.0 405 Method Not Allowed");
+        header("HTTP/1.0 405 Method Not Allowed");
         break;
 }
